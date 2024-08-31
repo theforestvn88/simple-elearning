706148f076c3ee4d2062e87a0f39002952dc0ac3
theforestvn88 <theforestvn88@gmail.com>
2024-08-31 17:32:48 +0700

    frontend: assignments list

--- app/javascript/components/Assignments.jsx
+++ app/javascript/components/Assignments.jsx
@@ -1,9 +1,63 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+import { useAppContext } from "../context/AppProvider"
+import Paginaton from "./Pagination"
 
 const Assignments = () => {
+    const { subject, identify, RequireAuthorizedApi } = useAppContext()
+    const [assignmentsData, setAssignmentsData] = useState({ assignments: [], pagination: null })
+    const [currPage, setCurrPage] = useState(1)
+
+    useEffect(() => {
+        RequireAuthorizedApi('GET', `/api/v1/${subject}/${identify}/assignments`, {}, {page: currPage})
+            .then((response) => {
+                if (response.ok) {
+                    return response.json()
+                }
+
+                throw new Error('Something went wrong!')
+            })
+            .then((responseAssignmentsData) => {
+                setAssignmentsData(responseAssignmentsData)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }, [currPage])
+
+    const AssignmentLink = ({assignment}) => (
+        <tr key={assignment.id}>
+            <td>
+                <Link to={`../${assignment.assignable_path}`}>
+                    You're assigned to [{`${assignment.assignable_type}.${assignment.assignable_id}] ${assignment.assignable_name}`}
+                </Link>
+            </td>
+            <td>{assignment.created_time}</td>
+        </tr>
+    )
+    
     return (
         <>
-            <h5>Your Assignments</h5>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Assignment</th>
+                        <th>Time</th> 
+                    </tr>
+                </thead>
+                <tbody>
+                    {assignmentsData.assignments.map((assignment) => <AssignmentLink assignment={assignment} />)}
+                </tbody>
+            </table>
+
+            <br />
+
+            {assignmentsData.pagination && 
+                <Paginaton
+                    pages={assignmentsData.pagination.pages} 
+                    currentPage={currPage} 
+                    selectedPage={setCurrPage} 
+                    totalPage={assignmentsData.pagination.total} />}
         </>
     )
 }
---
+++ app/javascript/test/components/Assignments.test.jsx
@@ -0,0 +1,45 @@
+import React, { act } from 'react'
+import { fireEvent, render, screen, cleanup } from '@testing-library/react'
+import { fetchMock, fetchMockReturn } from '../mocks/fetchMock'
+import Assignments from '../../components/Assignments'
+import { MemoryRouter } from 'react-router-dom'
+import AppProvider from '../../context/AppProvider'
+
+describe('Assignments', () => {
+    const fakeAssignments = [
+        {
+            id: 1,
+            assignable_type: 'Course',
+            assignable_id: 1,
+            assignable_name: 'React Tutorial',
+            assignable_path: '/courses/1',
+            created_time: '1 day',
+            updated_time: 'less than a minute'
+        }
+    ]
+    const FakePagination = { pages: ['1', '2', 'gap', '3', '4'], total: 10 }
+
+    beforeEach(() => {
+        fetchMockReturn({assignments: fakeAssignments, pagination: FakePagination })
+    })
+
+    afterEach(() => {
+        jest.clearAllMocks()
+        jest.restoreAllMocks()
+    })
+
+    
+    it('show assignments list', async () => {
+        await act( async () => render(<MemoryRouter><AppProvider subject='instructor' identify='meta'><Assignments /></AppProvider></MemoryRouter>))
+
+        expect(fetchMock).toHaveBeenCalledWith(
+            '/api/v1/instructor/meta/assignments?page=1', 
+            {"body": null, "headers": {"Content-Type": "application/json"}, "method": "GET"}
+        )
+
+        fakeAssignments.forEach((assignment) => {
+            expect(screen.getByRole('link', {to: assignment.assignable_path})).toBeInTheDocument()
+            expect(screen.getByText(assignment.created_time)).toBeInTheDocument()
+        })
+    })
+})
